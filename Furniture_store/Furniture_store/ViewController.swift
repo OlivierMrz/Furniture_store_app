750e9b50717513391caeb57249d4c2df573bca7b
Olivier Miserez <olivierm@avio.aero>
2021-06-11 15:49:58 +0200

    Initial commit

---
+++ Furniture_store/Furniture_store/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Furniture_store
+//
+//  Created by Olivier Miserez on 11/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+}
